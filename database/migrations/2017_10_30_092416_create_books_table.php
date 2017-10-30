ef31d93d82cf013946efd3e97b33e70ac6fdc103
Matěj Polák <matpolak@seznam.cz>
2017-10-30 10:35:18 +0100

    Created migrations (books, authors), Models (Book, Author), and Auth

---
+++ database/migrations/2017_10_30_092416_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('author_id')->nullable();
+            $table->string('title', 127);
+            $table->integer('published_at')->nullable();
+            $table->date('finished_reading_at')->nullable();
+            $table->string('my_review')->nullable();
+            $table->integer('my_rating')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
